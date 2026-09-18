b03e26fab90882ae37158f8fd6ffe09e27feb493
tunucaleb969-crypto <tunucaleb969@gmail.com>
2026-09-18 20:46:50 +0000

    Fix build failure: ContentScale is in androidx.compose.ui.layout, not androidx.compose.ui.graphics

--- app/src/main/java/com/carl/editor/timeline/ClipThumbnailStrip.kt
+++ app/src/main/java/com/carl/editor/timeline/ClipThumbnailStrip.kt
@@ -14,7 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ContentScale
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import android.graphics.Bitmap
